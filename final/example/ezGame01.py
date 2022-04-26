3abbd8e473e59d18efde4c7d04cb64b07ab45728
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-04-26 13:56:45 -0500

    working on final game

--- final/example/ezGame01.py
+++ final/example/ezGame01.py
@@ -1,5 +1,34 @@
 direction = ''
+####################################################################
+#  Constants
+####################################################################
+border = print('\n', 50*'=', '\n', 50*'-', '\n', 50*'.','\n')
+NAME = ''
+
+####################################################################
+#  Intro to the story
+####################################################################
+print('Welcome to the lands of Fluidity.', '\n', 'Where all is in constant motion and the things you have learned have sometimes never even happened.')
+
+####################################################################
+# User NAME
+####################################################################
+NAME = input('By what NAME shall I address you? ')
+
+
+###################################################################
+# DICTIONARY for 'player' data
+###################################################################
+def player():    
+    player = {'name':NAME, 'Health':10, 'has sword':False, 'has key':False, 'solved puzzle':0}
+    print(border)
+    print(NAME, 'You have begun your journey with', player['Health'], 'HEALTH')
+
+######################################################################
+#
+######################################################################
 while direction != 'q':
+    print()
     print('\n', 'You see portals that lie to the North "n", South "s", East "e", and West "w".')
     direction = input('Which portal do you choose? "n", "s", "e", or "w"? press "q" to quit.')
     
