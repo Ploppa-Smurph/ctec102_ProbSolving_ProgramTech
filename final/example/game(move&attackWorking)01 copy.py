24119bfe3a56e719e667e9b7741347bc57049501
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2022-05-02 19:04:12 -0500

    Final Project Paper completed

--- final/example/game(move&attackWorking)01 copy.py
+++ final/example/game(move&attackWorking)01 copy.py
@@ -26,7 +26,7 @@ NAME = input('\nBy what NAME shall I address you? ')
 # DICTIONARY for 'player' data
 ###################################################################
 def player():    
-    player = {'name':NAME, 'Health':10, 'has potion': False, 'has cheese': False, 'has torch': False, 'has sword':False, 'has key':False, 'solved puzzle':True}
+    player = {'name':NAME, 'Health':10, 'has sword':False, 'has key':False, 'solved puzzle':True}
     print(border)
     print('\n' + NAME, 'You have begun your journey with', player['Health'], 'HEALTH')
     return player
@@ -34,17 +34,17 @@ def player():
 #########################################################################
 # SET for Realm 
 #########################################################################
-realm_Set = {'Ethiopia', 'Constantanoble', 'Unified Sphere HQ','Dansk','Swansea','Japan', 'Rome', 'Pacific', 'Portland', 'New Buenos Aires'}
+realm_Set = {'Ethiopia', 'Constantanoble', 'Unified Sphere HQ','Dansk','Swansea','Japan', 'Rome', 'Buenos Aires'}
 
 ############################################################################
 # DICTIONARY for Puzzles
 ############################################################################
-puzzle = {'What is black, white and read all over? ': 'a Newspaper', 'Something about makes more of you' : 'a Mirror', 'Somthing about something' : 'Time'}
+puzzle = {'What is black, white and read all over? ': 'a Newspaper'}
 
 ############################################################################
 # List for Items
 ############################################################################
-item = ['sword', 'key', 'potion', 'torch', 'cheese']
+item = ['sword', 'key']
 
 ############################################################################
 # Creature Class
@@ -96,61 +96,52 @@ def move():
 # Navigation 
 ######################################################################
 turn = 0
+
 while direction != 'q':
-    print()
     print('\n', 'You see portals that lie to the North, South, East, and West.')
     direction = input('\nWhich portal do you choose? "n", "s", "e", or "w"? "q" to Quit and "i" for Information:  ')
-    
-    
-    if direction.lower() == 'n' or 's' or 'e' or 'w':
+    if direction.lower() == 'n':
         move()
         turn += 1
         print('Turn #: ', turn)
-        player()
-    elif direction.lower() == 'q':  # quit
-        print('\nYou have chosen to quit the game.')
-    elif direction.lower() == 'i':
-        player()
-    else:
-        print('\nYou have chosen an invalid direction, please choose "n", "s", "e", "w", "q", or "i".')  # invalid direction        
-    '''elif direction.lower() == 's':
+    elif direction.lower() == 's':
         move()
         turn += 1
-        player()
         print('Turn #: ', turn)
     elif direction.lower() == 'e':
         move()
         turn += 1
-        player()
-        print('Turn #: ', turn)  
+        print('Turn #: ', turn)
     elif direction.lower() == 'w':
         move()
         turn += 1
-        player()
-        print('Turn #: ', turn)'''
+        print('Turn #: ', turn)
+    elif direction.lower() == 'q':  # quit
+        print('\nYou have chosen to quit the game.')
+    elif direction.lower() == 'i':
+        print(player)
+    else:
+        print('\nYou have chosen an invalid direction, please choose "n", "s", "e", "w", "q", or "i".')  # invalid direction        
     
         
 #######################################################################################################################
 # Encounters
 #######################################################################################################################        
     if turn == puzTimer:
-                    
-        puzPop = puzzle.popitem()
         print('\nThere is something interesting in the  distance')
-        puzzAnswer = input('\nA jester appears and blocks your way. He smiles and looks you in the eyes and states. I have a puzzle for you, ' + str(puzPop)) 
-        if puzzAnswer.lower == puzPop[1]:
+        puzzAnswer = input('\nA jester appears and blocks your way. He smiles and looks you in the eyes and states. I have a puzzle for you, ' + puzzle[0]) 
+        if puzzAnswer.lower == puzzle[1]:
             puzzle['solved puzzle'] = True
             print('\nYou have solved the puzzle and have gained a puzzle point')
-        puzTimer = random.randint(16, 24)
+        puzTimer = -99
         
     elif turn == itemTimer:
         itemPop = item.pop()
         print('\nThere is something of use in the distance')
-        itemQuiz = input('\nA bauble catches your eye, it is a ' + itemPop)
-        if itemQuiz.lower() == itemPop:
-            item['has ' + itemPop] = True
-            print('\nYou have gained an item')
-        itemTimer = random.randint(11, 18)
+        print('\nA bauble catches your eye, it is a ' + itemPop)
+        player['has ' + itemPop] = True
+        print('\nYou have gained an item')
+        itemTimer = random.randint(itemTimer + 2, itemTimer + 8)
     else:
         move() 
     '''elif turn == encounter:
